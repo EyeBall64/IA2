609ba6375f120ebfb22d82bb4e5b6a256c429d72
16Hurley_J <jpjhurley@gmail.com>
2022-07-07 10:15:42 +0100

    finsihed table(ish)

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -10,72 +10,82 @@ public class GUIMain extends JFrame implements ActionListener, DocumentListener 
     private JPanel testMenu;
     private JPanel tablePanel;
 
-
-
     public GUIMain() {
-
         mainFrame = new JFrame("Latin Noun Table Revision");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setBounds(0, 0, 365, 250);
         mainFrame.setLayout(null);
 
-
         //TODO: Add all panels, start hidden
 
         mainMenu();
         testMenu();
+        table();
+
+        mainMenu.setVisible(true);
+        testMenu.setVisible(false);
+        tablePanel.setVisible(false);
+
+
         mainFrame.setVisible(true);
     }
 
-    private void mainMenu(){ //TODO: repeat for other panels
+    private void mainMenu(){
         mainMenu = new JPanel();
         mainMenu.setBounds(0, 0, 365, 250);
+        mainMenu.setLayout(null);
 
         //test button
         JButton testButton = new JButton("Test");
-        testButton.setBounds(20, 20, 50,30);
+        testButton.setBounds(20, 20, 70,30);
         testButton.addActionListener(this);
         mainMenu.add(testButton);
-        mainFrame.add(mainMenu);
 
         //table button
         JButton tableButton = new JButton("Table");
-        tableButton.setBounds(100, 20, 50,30);
+        tableButton.setBounds(100, 20, 70,30);
         tableButton.addActionListener(this);
         mainMenu.add(tableButton);
+
         mainFrame.add(mainMenu);
     }
 
     private void testMenu(){
         testMenu = new JPanel();
         testMenu.setBounds(0, 0, 365, 250);
+        testMenu.setLayout(null);
 
         //noTerm Button
-        JButton noTermButton = new JButton("Test");
-        noTermButton.setBounds(20, 20, 50,30);
+        JButton noTermButton = new JButton("no");
+        noTermButton.setBounds(20, 20, 70,30);
         noTermButton.addActionListener(this);
         testMenu.add(noTermButton);
-        mainFrame.add(testMenu);
 
         //shortTerm Button
-        JButton shortTermButton = new JButton("Leave");
-        shortTermButton.setBounds(100, 20, 50,30);
+        JButton shortTermButton = new JButton("short");
+        shortTermButton.setBounds(100, 20, 70,30);
         shortTermButton.addActionListener(this);
         testMenu.add(shortTermButton);
-        mainFrame.add(testMenu);
 
         //longTerm Button
-        JButton longTermButton = new JButton("Table");
-        longTermButton.setBounds(150, 20, 50,30);
+        JButton longTermButton = new JButton("long");
+        longTermButton.setBounds(150, 20, 70,30);
         longTermButton.addActionListener(this);
         testMenu.add(longTermButton);
+
+        //leave Button
+        JButton leaveButton = new JButton("Leave");
+        leaveButton.setBounds(100, 100, 70,30);
+        leaveButton.addActionListener(this);
+        testMenu.add(leaveButton);
+
         mainFrame.add(testMenu);
     }
 
     private void table(){
-
         tablePanel = new JPanel();
         tablePanel.setBounds(0, 0, 365, 250);
+        tablePanel.setLayout(null);
         Table table = new Table();
 
         String[] columnTitles = new String[9];
@@ -84,23 +94,23 @@ public class GUIMain extends JFrame implements ActionListener, DocumentListener 
         for (int y = 0; y < 9; y++) {
             columnTitles[y] = table.getTable(0,y);
         }
-        for (int x = 1; x < 13; x++) {
+        for (int x = 0; x < 12; x++) {
             for (int y = 0; y < 9; y++) {
-                grid[x - 1][y] = table.getTable(x,y);
+                grid[x][y] = table.getTable(x,y);
+                JLabel tableLabel = new JLabel(table.getTable(x,y));
+                tableLabel.setBounds(y*100, 70+x*20, 100,30);
+                tablePanel.add(tableLabel);
+                mainFrame.add(tableLabel);
             }
         }
 
-       //Table table
-        JTable tableTable = new JTable(grid,columnTitles);
-        tableTable.setBounds(150, 20, 50,30);
-        testMenu.add(tableTable);
-        mainFrame.add(tablePanel);
+
 
         //leave Button
         JButton leaveButton = new JButton("Leave");
-        leaveButton.setBounds(100, 20, 50,30);
+        leaveButton.setBounds(100, 20, 70,30);
         leaveButton.addActionListener(this);
-        testMenu.add(leaveButton);
+        tablePanel.add(leaveButton);
         mainFrame.add(tablePanel);
     }
 
@@ -116,14 +126,12 @@ public class GUIMain extends JFrame implements ActionListener, DocumentListener 
                 mainMenu.setVisible(false);
                 testMenu.setVisible(true);
                 tablePanel.setVisible(false);
-                testMenu();
             }
             case "Leave" ->{
                 //goes to main Menu
                 mainMenu.setVisible(true);
                 testMenu.setVisible(false);
                 tablePanel.setVisible(false);
-                mainMenu();
             }
             case "Table" -> {
                 //goes to table
